140f44e3467e4994cbbca471441a73e8b04f7340
ynavach <yunensey@hotmail.com>
2023-03-14 23:02:53 +0000

    My third commit

--- src/models.py
+++ src/models.py
@@ -33,7 +33,6 @@ class Follower(Base):
     __tablename__ = "follower"
 
     id = Column(Integer, primary_key=True)
-#    user_from_id = Column(Integer, primary_key=True)
     user_to_id = Column(Integer)
     user_id = Column(ForeignKey("user.id"))
 
